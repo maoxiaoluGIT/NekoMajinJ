c701d9c198bb86af1266a8bee5b3e7178ffd3ba7
maoxiaoluGIT <16624478@qq.com>
2018-10-24 16:12:28 +0800

    hello world !

---
+++ src/com/nekomajinj/GameMain.java
@@ -0,0 +1,9 @@
+package com.nekomajinj;
+
+public class GameMain {
+	//https://github.com/maoxiaoluGIT/NekoMajinJ.git
+	public static void main(String[] args) {
+		System.out.println("hello world !");
+	}
+
+}
